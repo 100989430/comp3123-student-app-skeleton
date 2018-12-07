7cb3e7e487dcb2094135c1479ad829a034092433
Karthik Shankar Prasad <100989430@georgebrown.ca>
2018-12-06 20:23:56 -0500

    mongoose dev

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
 const app = express();
 
 // *********** Include the Api routes ***********
@@ -10,7 +11,7 @@ const studentRoutes = require("./routes/students");
 // *********** Connect to Mongo  ***********
 console.log('Attempting to connect to mongoose');
 
-mongoose.connect('mongodb://localhost:27017/host')
+mongoose.connect('mongodb://admin:College1@ds123400.mlab.com:23400/movies')
   .then(() => {
     console.log('Connected to Mongo database!');
   })
@@ -35,3 +36,71 @@ app.use((req, res, next) => {
 app.use("/api/students", studentRoutes);
 
 module.exports = app;
+
+
+var Schema = mongoose.Schema;
+var movieSchema  = new Schema({id:Number, title:String, genre: String});
+var Movie = mongoose.model('Movie',movieSchema);
+
+
+var favMovie =  Movie({id: 1, title: "Lord Of The Rings", genre: "Fantasy",id: 2, title: "Scream", genre: "Horror"}, )
+/*favMovie.save(function(err)
+{
+  if(err) throw err;
+
+  console.log('****Movie Saved***');
+
+})
+*/
+
+
+
+
+
+
+//get one movies
+/*
+Movie.find({id:1}, function(err, movies)
+{
+  if(err) throw err;
+ 
+ console.log('***********My fav movies is:')
+  console.log(movies);
+
+})
+*/
+
+//delete one movie
+/*
+Movie.remove({id:1}, function(err, movies)
+{
+  if(err) throw err;
+ 
+ 
+  console.log(movies);
+
+})
+*/
+
+//update one movie
+
+Movie.findOneAndUpdate({id:1},{title:"Hello class"}, function(err, movies)
+{
+  if(err) throw err;
+ 
+ 
+  console.log("Updated");
+})
+
+
+Movie.findOneAndDelete({id:4},{title:"Scream"}, function(err, movies)
+{
+  if(err) throw err;
+ 
+ 
+  console.log("Deleted");
+})
+
+
+
+
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { isNgTemplate } from '@angular/compiler';
 import { iterateListLike } from '@angular/core/src/change_detection/change_detection_util';
-
-const FAKE_DB: any[] = [
+export const FAKE_DB: any[] = [
 
   { id: 11, title: 'Fast & Furious', genre: 'Fantasy'},
   
   { id: 22, title: 'Scream', genre: 'Horror'},
   
   { id: 33, title: 'Star Wars', genre: 'Fiction'},
-  { id: 44, title: 'Avengers', genre: 'Action'}
+  { id: 44, title: 'Avengers', genre: 'Action'},
+  { id: 55, title: 'Terminator3', genre: 'Fantasy'}
   
   ];
+  
   
 
 @Injectable({
